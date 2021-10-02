c67f4c8eead3737fa36617a7568f4b77281da9ae
enmalafeev <malafeev.en@yandex.ru>
2021-10-03 00:03:21 +0300

    add validators

--- src/controllers/addImage.js
+++ src/controllers/addImage.js
@@ -1,15 +1,15 @@
 const db = require('../entities/Database');
 const Image = require('../entities/Image');
-// const { BadRequestApiError } = require('../../validators/errors/ApiError');
+const { BadRequestApiError } = require('../../validators/errors/ApiError');
 
 module.exports = async (req, res, next) => {
   try {
     const { imageId, size } = req.file;
     const uploaded = Date.now();
 
-    // if (!content) {
-    //   throw new BadRequestApiError('SVG content should not be empty');
-    // }
+    if (!req.file) {
+      throw new BadRequestApiError('Image content should not be empty');
+    }
 
     const imageFile = new Image(imageId, size, uploaded);
 

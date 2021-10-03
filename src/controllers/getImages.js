f23b6c36771a7a61b7b5b576c25683040bde89ad
enmalafeev <malafeev.en@yandex.ru>
2021-10-03 13:51:30 +0300

    fix getImages on array

--- src/controllers/getImages.js
+++ src/controllers/getImages.js
@@ -2,5 +2,5 @@ const db = require('../entities/Database');
 
 module.exports = (req, res) => {
   const allImages = db.find().map((img) => ({ id: img.id, size: img.size, uploadedAt: img.uploadedAt }));
-  return res.json({ allImages });
+  return res.json(allImages);
 };

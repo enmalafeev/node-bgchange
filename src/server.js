55eee45be622b4760d5a33dea96820850b760278
enmalafeev <malafeev.en@yandex.ru>
2021-10-03 14:10:07 +0300

    add middlewares

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const { PORT } = require('./config');
 const { mainRouter } = require('./routers');
+const setupMiddlewares = require('./middlewares');
+const errorHandler = require('./middlewares/errorHandler');
 
 
 const app = express();
 
+setupMiddlewares(app);
+
 app.use('uploads', express.static('uploads'));
 
 app.use('/', mainRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
